db4c622c7774a47b956ac4d3e3fbabb4f05635c4
Tkachenko Yevhen <zekatka01@gmail.com>
2023-01-11 12:22:50 +0200

    fetching

--- src/components/InfoItem/InfoItem.tsx
+++ src/components/InfoItem/InfoItem.tsx
@@ -24,7 +24,7 @@ export const InfoItem = ({icon, text, isLink}: InfoItemProps) => {
                        target='_blank'
                        rel='noreferrer'
                        className={styles.link}>
-                        currentText
+                        {currentText}
                     </a>
                 ) : currentText}
             </div>
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -2,7 +2,6 @@ import { LocalGitHubUser} from "../types";
 
 export  const defaultUser: LocalGitHubUser = {
     "login": "Famaiii",
-    "id": 80358296,
     "avatar": "https://avatars.githubusercontent.com/u/80358296?v=4",
     "name": "Yevhen",
     "company": null,
